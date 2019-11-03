24ee36ae0f5546282806f423da2b35b0aa2c1d12
crr <crr@example.com>
2019-11-03 12:40:05 +0100

    adding get and post axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
-import Layout from '../src/containers/Layout/Layout';
+import Layout from '../src/hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 
 class App extends Component {
+
   render(){
     return (
       <div>
