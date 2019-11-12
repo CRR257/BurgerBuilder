afd79035e1dfa53bf85508367cd54343333959fe
crr <crr@example.com>
2019-11-12 21:23:38 +0100

    adding form submit

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -11,19 +11,22 @@ const input = (props) => {
             inputElement = <input 
                 className="InputElement" 
                 {...props.elementConfig} 
-                value={props.value} />;
+                value={props.value} 
+                onChange={props.changed} />;
             break;
         case ('textarea'):
             inputElement = <textarea 
                 className="InputElement" 
                 {...props.elementConfig} 
-                value={props.value} />
+                value={props.value} 
+                onChange={props.changed} />
             break;
         case ('select'): 
             inputElement = (
                 <select
                     className="InputElement" 
-                    value={props.value}>
+                    value={props.value} 
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option 
                             key={option.value}
@@ -38,7 +41,7 @@ const input = (props) => {
             inputElement = <input 
                 className="InputElement" 
                 {...props.elementConfig} 
-                value={props.value} />;
+                value={props.value} onChange={props.changed} />;
             break;
     }
  
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -70,10 +70,17 @@ class ContactData extends Component {
         event.preventDefault();
         console.log(this.props.ingredients, this.props.price)
         this.setState({loading: true});
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
+            orderData: formData
         }
+        
         axios.post('/orders.json', order)
             .then(response => {
                 this.setState({loading: false});
@@ -86,6 +93,20 @@ class ContactData extends Component {
             })  
     }
 
+    inputChangedHandler = (event,inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+            //we are cloning deeply the object
+        };
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        this.setState({orderForm: updatedOrderForm})
+    }
+
+
     render() {
         const formElementsArray = [];
         for (let key in this.state.orderForm) {
@@ -96,15 +117,16 @@ class ContactData extends Component {
         }
 
         let form = (
-            <form>
+            <form onSubmit={this.orderHandler}>
                 {formElementsArray.map(formElement => (
                     <Input 
                         key={formElement.id}
                         elementType={formElement.config.elementType}
                         elementConfig={formElement.config.elementConfig}
-                        value={formElement.config.value} />
+                        value={formElement.config.value} 
+                        changed={(event) => this.inputChangedHandler(event,formElement.id)}/>
                 ))}
-                <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
+                <Button btnType="Success">Order</Button>
             </form>   
         );
 
